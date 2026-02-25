9581acc90c027fb013c152031dca903f0f6cb71b
pawored <pawored0oo@gmail.com>
2026-02-25 09:40:57 +0100

    Delete Inserts directory

--- Inserts/regiones.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO REGION (nombre, siglas, plazas_mundial) VALUES
-('North America',              'NA',   3),
-('Europe',                     'EU',   3),
-('South America',              'SAM',  2),
-('Middle East & North Africa', 'MENA', 2),
-('Oceania',                    'OCE',  1),
-('Sub-Saharan Africa & Korea', 'SSKC', 1);
