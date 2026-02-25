44b7e4db9fb6e0605560234718a0c2e06bf1894c
pawored <pawored0oo@gmail.com>
2026-02-25 09:41:15 +0100

    Add files via upload

---
+++ sql/inserts/regiones.sql
@@ -0,0 +1,7 @@
+INSERT INTO REGION (nombre, siglas, plazas_mundial) VALUES
+('North America',              'NA',   3),
+('Europe',                     'EU',   3),
+('South America',              'SAM',  2),
+('Middle East & North Africa', 'MENA', 2),
+('Oceania',                    'OCE',  1),
+('Sub-Saharan Africa & Korea', 'SSKC', 1);
